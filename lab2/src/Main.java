3694a8bea54dabac01218c671962b15145c70407
zhuravlevv <vlad12.09.01.1@mail.ru>
2021-03-02 13:02:49 +0300

    add validation

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -21,6 +21,11 @@ public class Main {
         for (String function :
                 functions) {
             if (function.contains(")") && function.contains("(")) {
+                function = deleteSpaces(function);
+                if(function.charAt(function.length() - 1) != ')'){
+                    cleanConsole();
+                    throw new Exception("Syntax error!");
+                }
 
                 int left = function.indexOf("(");
                 int right = function.indexOf(")");
@@ -28,18 +33,41 @@ public class Main {
 
                 String allParams = function.substring(left + 1, right);
 
+                functionName = deleteSpaces(functionName);
+                allParams = deleteSpaces(allParams);
+
+                if (functionName.contains(" ")
+                        || functionName.contains("\t")
+                        || functionName.contains("\n")
+                        || functionName.contains("\\")) {
+
+                    cleanConsole();
+                    throw new Exception("Spaces on Function Name");
+                }
+
                 System.out.println(functionName + "\t\t\tИмя процедуры");
 
 
                 String[] params = allParams.split(",");
                 for (String param:
-                     params) {
+                        params) {
+                    param = deleteSpaces(param);
                     if(!param.isEmpty()) {
+                        if (param.contains(" ")
+                                || functionName.contains("\t")
+                                || functionName.contains("\n")
+                                || functionName.contains("\\")) {
+
+                            cleanConsole();
+                            throw new Exception("Spaces on Param Name");
+                        }
+
                         if (isParamRomanNumeral(param)) {
                             System.out.println(param + "\t\t\tРимская цифра");
                         } else {
                             System.out.println(param + "\t\t\tИдентификатор");
                         }
+
                     }
 
                 }
@@ -69,4 +97,22 @@ public class Main {
             return false;
     }
 
+    public static String deleteSpaces(String string){
+        if(string.isEmpty())
+            return "";
+        while(string.indexOf(" ") == 0){
+            string = string.replaceFirst(" ", "");
+        }
+        if(string.isEmpty())
+            return "";
+        while(string.lastIndexOf(" ") == string.length() - 1){
+            string = string.substring(0, string.length() - 1);
+        }
+        return string;
+    }
+
+    public static void cleanConsole(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 }
